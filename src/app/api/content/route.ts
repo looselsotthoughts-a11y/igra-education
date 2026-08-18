21d5579e813dbd404388abcbe17f378946680c09
Aplha88Q <pandeylakshya2004@gmail.com>
2026-08-18 11:29:54 +0530

    Disable Caching, force refreshing

--- src/app/api/content/route.ts
+++ src/app/api/content/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
 
+export const revalidate = 0;
+
 export async function GET() {
   const { data, error } = await supabase
     .from("content")
